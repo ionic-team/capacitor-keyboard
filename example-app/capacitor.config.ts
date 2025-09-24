97d1ba4eae95e663544e05cb112523d1ea791e3c
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2025-09-24 12:03:44 -0300

    chore: add example-app (#2)
    * chore: add example-app
    
    Context: App is based on https://github.com/ionic-team/capacitor-testapp, but we did a few improvements.
    
    * chore(release): 8.0.0-next.1 [skip ci]
    
    This reverts commit ddb52e6d36136c7dc5f27cebc2932447e8858f19.
    
    * chore: update plugin version in example-app

---
+++ example-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CAP-KEYB-SAMPLE',
+  webDir: 'dist'
+};
+
+export default config;
